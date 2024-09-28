c29cbcef05fc7a45400229be475878a05a63a013
sumudu <sumudutharaka196@gmail.com>
2024-09-28 17:26:51 +0530

    Init project

---
+++ index.js
@@ -0,0 +1,7 @@
+// document.getElementById('buyNowBtn').onclick = () => {
+//     alert('Hello World')
+// };
+
+// document.getElementById('buyNowBtn').addEventListener('click', () => {
+//     alert("Hey There")
+// })
